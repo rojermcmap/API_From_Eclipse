9da3ba3084deff447f567fc91d433975f12cf60d
Michael <rojermcmap@gmail.com>
2020-04-28 18:23:55 +0530

    Eclipse Edit Pish

--- JavaExamples/src/test/java/absclas/ABBSCLS.java
+++ JavaExamples/src/test/java/absclas/ABBSCLS.java
@@ -5,7 +5,7 @@ package absclas;
 public class ABBSCLS extends Sclass{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub1
 		
 	
 		
